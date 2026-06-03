ebea88efa5ceb231fc543863167cb60fbf3e0e33
Nicolas Vella <nicolasvella10@gmail.com>
2026-06-03 14:54:57 +0200

    Ajout du layout du menu latéral de l'espace admin.

--- src/app/(app-avec-header)/(dashboard)/admin/layout.tsx
+++ src/app/(app-avec-header)/(dashboard)/admin/layout.tsx
@@ -1,9 +1,10 @@
-// * src/app/(app-avec-header)/(dashboard)/membre/layout.tsx
+// * src/app/(app-avec-header)/(dashboard)/admin/layout.tsx
+
 import React from 'react';
-import MemberSideMenu from '@/components/MemberSideMenu';
+import AdminSideMenu from '@/components/AdminSideMenu';
 
 /**
- * Layout unique de l'espace membre. 
+ * Layout unique de l'espace admin. 
  * Il applique le menu fixe à gauche et injecte les pages à droite via {children}.
  * Utilise une marge fixe à gauche pour empêcher le contenu de se superposer sous le menu.
  */
@@ -18,7 +19,7 @@ export default function MembreLayout({
 
             {/* Barre latérale fixe à gauche (Prend strictement 1/6 de la largeur) */}
             <aside className="w-1/6 fixed inset-y-16 left-0 bg-white border-r border-slate-200 z-40 p-4">
-                <MemberSideMenu />
+                <AdminSideMenu />
             </aside>
 
             {/* Zone de droite décalée de force par une marge équivalente à la largeur du menu (1/6 = 16.666667%) */}
