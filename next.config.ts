5877f51dfd49c84fddcbd07714060d5ec5fb08ed
Tanguy Matteo <tanguymatteo.pro@gmail.com>
2026-06-12 14:41:28 +0200

    redirection page accueil

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
   serverExternalPackages: ['pg', '@prisma/adapter-pg'],
+
+  async redirects() {
+    
+    return [
+      {
+        source: '/',                      
+        destination: '/accueil',  
+        permanent: true,                  
+      },
+    ];
+  },
 };
 
 export default nextConfig;
