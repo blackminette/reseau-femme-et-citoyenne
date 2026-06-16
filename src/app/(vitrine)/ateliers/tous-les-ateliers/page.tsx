8bf5d11c26aa7465d31470df1db731a2ae9ca79d
Daria <dariaflow06@ex.com>
2026-06-16 10:20:27 +0200

    feat: developpement de la page tous les ateliers

--- src/app/(vitrine)/ateliers/tous-les-ateliers/page.tsx
+++ src/app/(vitrine)/ateliers/tous-les-ateliers/page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 import { Quicksand } from 'next/font/google';
 
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
 const quicksand = Quicksand({ 
     subsets: ['latin'],
     weight: ['400', '700'] 
@@ -37,41 +40,60 @@ const listeAteliers = [
 
 export default function TousNosAteliers(){
     return (
-        // fond violet
-        <div className='bg-[#eedeff] min-h-screen py-16 px-4'>
-            <div className='max-w-5xl mx-auto'>
+        <>
+            {/* Header */}
+            <Header />
 
-                {/* Titre principal */}
-                <h1 className={`${quicksand.className} text-5xl font-bold text-center text-[#260936] mb-24`}>Tous nos Ateliers</h1>
+            {/* Fond violet */}
+            <div className='bg-[#eedeff] min-h-screen py-16 px-4'>
+                <div className='w-7xl mx-auto'>
 
-                {/* Grille avec les encadres des ateliers */}
-                <div className='grid grid-cols-2 gap-x-16 gap-y-20 justify-items-center'>
-                    {listeAteliers.map((atelier) => (
-                        <div key={atelier.id} className='bg-[#bc96e6] w-full max-w-md aspect-[4/3] p-8 flex flex-col justify-between items-center text-center shadow-md rounded-sm border border-[#752fbb] shadow-black'>
-                            
-                            {/* Encadré pointillé pour l'image */}
-                            <div className="w-full h-1/2 border border-dashed border-black flex flex-col justify-center items-center"></div>
+                    {/* Titre principal */}
+                    <h1 className={`${quicksand.className} text-5xl font-bold text-center text-[#260936] mb-24`}>Tous nos Ateliers</h1>
 
-
-                            {/* Bloc Texte : Titre + Description sous l'image */}
-                            <div className="flex flex-col gap-1 my-3 justify-center">
-                                <h3 className="text-lg font-semibold text-[#260936]">{atelier.titre}</h3>
-                                <p className="text-sm font-normal text-[#260936]-600 line-clamp-2 px-2">{atelier.description}</p>
-                            </div>
+                    {/* Grille avec les encadres des ateliers */}
+                    <section className="mx-8 my-32 rounded-[2.5rem] bg-[#752fbb] p-8 md:p-20 text-center text-white shadow-2xl max-w-[90rem] xl:mx-auto relative">
+                        <div className="absolute top-0 left-0 w-full h-full opacity-5 pointer-events-none">
+                            <svg width="100%" height="100%">
+                                <pattern id="grid" width="40" height="40" patternUnits="userSpaceOnUse">
+                                    <path d="M 40 0 L 0 0 0 40" fill="none" stroke="white" strokeWidth="1" /></pattern>
+                                    <rect width="100%" height="100%" fill="url(#grid)" /></svg>
+                        </div> 
 
 
-                            {/* Bloc Prochain Atelier */}
-                            <Link href='#' //je dois remplacer plus tard avec le vrai lien 
-                            className="flex flex-col items-center gap-0.5 mt-auto group transition-all"
-                            >
-                                <p className="text-xs font-semibold text-[#752fbb] group-hover:text-[#260936]">{atelier.prochainAtelier}</p>
-                                <span className="text-xs font-light text-[#752fbb] group-hover:text-[#260936] mt-0.5 font-mono">v</span>
-                            </Link>
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-8 w-full justify-items-center'>
+                            {listeAteliers.map((atelier) => (
+                                <div key={atelier.id} className='bg-[#bc96e6]/20 rounded-2xl overflow-hidden w-full max-w-md aspect-[4/3] p-8 flex flex-col 
+                                justify-between items-center shadow-[inset_0_1px_2px_rgba(255,255,255,0.4)]'>
+
+                                    {/* Encadré pointillé pour l'image */}
+                                    <div className="w-full h-1/2 rounded-[0.3rem] bg-white/10 border border-dashed border-black flex flex-col justify-center items-center"></div>
+
+
+                                    {/* Bloc Texte : Titre + Description sous l'image */}
+                                    <div className="flex flex-col gap-1 my-3 justify-center">
+                                        <h3 className="text-lg font-semibold text-[#260936]">{atelier.titre}</h3>
+                                        <p className="text-sm font-normal text-white/80 line-clamp-2 px-2">{atelier.description}</p>
+                                    </div>
+
+
+                                    {/* Bloc Prochain Atelier */}
+                                    <Link href='/ateliers/planning' //je dois remplacer plus tard avec le vrai lien 
+                                    className="flex flex-col items-center gap-0.5 mt-auto group transition-all"
+                                    >
+                                        <p className="text-xs font-semibold text-[#ffd166] group-hover:text-[#260936]">{atelier.prochainAtelier}</p>
+                                        <span className="text-xs font-light text-[#ffd166] group-hover:text-[#260936] mt-0.5 font-mono">v</span>
+                                    </Link>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
+                    </section>
 
+                </div>
             </div>
-        </div>
+
+            {/* Footer */}
+            <Footer />
+        </>
     );
 }
