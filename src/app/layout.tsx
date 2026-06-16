f8c2214e2876769a11b474dab95d00839db67e5f
letruyenduc <letruyenduc@gmail.com>
2026-06-16 10:12:26 +0200

    Ajout header et footerg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 // * src/app/layout.tsx
 import React from 'react';
 import '@/app/globals.css';
-
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 export const metadata = {
     title: 'Mon Projet Association',
     description: 'Application de gestion avec contrôle des rôles (RBAC)',
@@ -18,9 +19,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="fr">
-            <body className="antialiased bg-slate-100 text-slate-900">
+            <body className="antialiased bg-slate-100 text-slate-900 flex flex-col min-h-screen">
                 {/* C'est ici que Next.js viendra injecter tes différentes pages */}
-                {children}
+                <Header />
+                <main className="grow">
+                    {children}
+                </main>
+                <Footer />
             </body>
         </html>
     );
