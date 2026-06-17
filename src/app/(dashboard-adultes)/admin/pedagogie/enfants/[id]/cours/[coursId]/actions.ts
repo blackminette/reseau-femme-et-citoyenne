18c631350c81c419c2089fdc49f04f1c7004bf55
Nicolas Vella <nicolasvella10@gmail.com>
2026-06-17 12:34:19 +0200

    copie de la logique de la section adulte vers la section enfant et changement mineur pour que tous correspondent

---
+++ src/app/(dashboard-adultes)/admin/pedagogie/enfants/[id]/cours/[coursId]/actions.ts
@@ -0,0 +1,48 @@
+// * src/app/(dashboard-adultes)/admin/pedagogie/enfants/[id]/cours/[coursId]/actions.ts
+'use server';
+
+import { prisma } from '@/lib/prisma';
+import { revalidatePath } from 'next/cache';
+
+export async function getCours(id: number) {
+    try {
+        const result = await prisma.cours.findUnique({
+            where: { id: id }
+        })
+        return { success: true, data: result };
+    } catch (error) {
+        return { success: false, error: "Erreur lors de la récupération du cours." }
+    }
+}
+
+export async function modifierTitreCours(coursId: number, editTitre: string, moduleId: number) {
+    try {
+        const result = await prisma.cours.update({
+            where: { id: coursId },
+            data: {
+                titre: editTitre
+            }
+        });
+
+        revalidatePath(`/admin/pedagogie/enfants/${moduleId}/cours/${coursId}`);
+
+        return { success: true }
+    } catch (error) {
+        return { success: false, error: "Erreur lors de la modification du titre du cours." }
+    }
+}
+
+export async function modifierContenuCours(coursId: number, nouveauContenue: any[], moduleId: number) {
+    try {
+        const result = await prisma.cours.update({
+            where: { id: coursId },
+            data: { contenu: nouveauContenue }
+        })
+
+        revalidatePath(`/admin/pedagogie/enfants/${moduleId}/cours/${coursId}`);
+
+        return { success: true }
+    } catch (error) {
+        return { success: false, error: "Erreur lors de la mise a jour du contenue." }
+    }
+}
